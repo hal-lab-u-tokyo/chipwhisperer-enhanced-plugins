15c8f9cfe2095a7c7669c4a95a86a37efd6d4a5b
tkojima.am <tkojima@am.ics.keio.ac.jp>
2025-04-30 05:16:51 +0900

    fix build problem due to build-isolation

--- setup.py
+++ setup.py
@@ -3,61 +3,56 @@ import subprocess
 import os
 from setuptools.command.develop import develop
 from setuptools.command.install import install
+from setuptools.command.build_ext import build_ext
+
 from pathlib import Path
 import glob
 import shutil
 
 import_name = "cw_plugins"
 
-try:
-    import pybind11
-except ImportError as e:
-    ext_modules = []
-    class CMakeBuild:
-        def __init__(self, *args):
-            super().__init__(*args)
-            raise RuntimeError("pybind11 is required to build the C++ extension.")
 
-        def run(self):
-            raise RuntimeError("pybind11 is required to build the C++ extension.")
-else:
-    from setuptools.command.build_ext import build_ext
+class CMakeExtension(Extension):
+    def __init__(self, name, cmake_src_dir):
+        super().__init__(name, sources=[])
+        self.cmake_src_dir = os.path.abspath(cmake_src_dir)
 
-    class CMakeExtension(Extension):
-        def __init__(self, name, cmake_src_dir):
-            super().__init__(name, sources=[])
-            self.cmake_src_dir = os.path.abspath(cmake_src_dir)
+class CMakeBuild(build_ext):
 
-    class CMakeBuild(build_ext):
+    def __init__(self, *args):
+        super().__init__(*args)
 
-        def __init__(self, *args):
-            super().__init__(*args)
+    def run(self):
+        # check if build directory exists
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
 
-        def run(self):
-            # check if build directory exists
-            if not os.path.exists(self.build_temp):
-                os.makedirs(self.build_temp)
+        abs_build_lib = os.path.abspath(self.build_lib)
 
-            abs_build_lib = os.path.abspath(self.build_lib)
+        for ext in self.extensions:
 
-            for ext in self.extensions:
+            cmake_args = [f'-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={abs_build_lib}{os.sep}{ext.name}']
 
-                cmake_args = [f'-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={abs_build_lib}{os.sep}{ext.name}']
+            build_dir = f"{self.build_temp}{os.sep}{ext.name}"
+            os.makedirs(build_dir, exist_ok=True)
+            CMAKE_CMD = "cmake3" if shutil.which("cmake3") else "cmake"
+            # configure
+            try:
+                subprocess.check_call([CMAKE_CMD, ext.cmake_src_dir] + cmake_args, cwd=build_dir)
+            except subprocess.CalledProcessError as e:
+                print("CMake configuration failed")
+                print(e)
+                return
+            # build
+            subprocess.check_call([CMAKE_CMD, '--build', '.', '-j', f"{os.cpu_count()}"], cwd=build_dir)
 
-                build_dir = f"{self.build_temp}{os.sep}{ext.name}"
-                os.makedirs(build_dir, exist_ok=True)
-                # configure
-                subprocess.check_call(['cmake3', ext.cmake_src_dir] + cmake_args, cwd=build_dir)
-                # build
-                subprocess.check_call(['cmake3', '--build', '.'], cwd=build_dir)
-
-            if self.inplace:
-                self.copy_tree(self.build_lib, f"lib")
+        if self.inplace:
+            self.copy_tree(self.build_lib, f"lib")
 
 
-    ext_modules = [
-        CMakeExtension(f"{import_name}{os.sep}analyzer{os.sep}attacks", 'cpp_libs')
-    ]
+ext_modules = [
+    CMakeExtension(f"{import_name}{os.sep}analyzer{os.sep}attacks", 'cpp_libs')
+]
 
 def post_process(installed_path):
     print("Search for hwh files of example designs")
@@ -146,7 +141,7 @@ setup(
         "numpy>=1.25.0",
         "ipyfilechooser",
         "pyelftools",
-        "nest_asyncio",
+        "nest_asyncio"
     ],
 
     packages=find_packages(where='lib',exclude=['notebooks']),
